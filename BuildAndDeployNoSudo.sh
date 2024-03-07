0533f261592baa481c2271c8f2249eda4433d308
shabuhabs <shabuhabs@gmail.com>
2024-03-06 20:14:20 -0800

    Updated dockerfiles with agent changes.

--- BuildAndDeployNoSudo.sh
+++ BuildAndDeployNoSudo.sh
@@ -1,9 +1,11 @@
-docker-compose down 
+docker compose down 
 
 mvn clean install
 
+docker network create instrument_shop
 
-docker-compose build --build-arg DD_GIT_REPOSITORY_URL=github.com/shabuhabs/instrumentShop \
+
+docker compose build --build-arg DD_GIT_REPOSITORY_URL=github.com/shabuhabs/instrumentShop \
 --build-arg DD_GIT_COMMIT_SHA=$(git rev-parse HEAD)
 
-docker-compose up -d
+docker compose up -d
--- test/src/main/java/GenerateTraffic.java
+++ test/src/main/java/GenerateTraffic.java
@@ -18,7 +18,7 @@ public class GenerateTraffic {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		try {
-		 Thread.sleep(3000);
+		 Thread.sleep(15000);
 		}catch(Exception e ){
 		}
 		 
